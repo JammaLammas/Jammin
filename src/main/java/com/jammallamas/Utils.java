016b88e38d2f06b727ab6514902cb04fbeb2efd2
timoreo <timo.oreo34@gmail.com>
2021-06-22 08:27:32 +0200

    removed that old useless function

--- src/main/java/com/jammallamas/Utils.java
+++ src/main/java/com/jammallamas/Utils.java
@@ -62,33 +62,6 @@ public class Utils {
         }
     }
 
-    /**
-     * Gets where it intersects
-     * only compute when the first intersects is true
-     *
-     * @param R1 the "player" renderable (or entity)
-     * @param R2 the tile renderable
-     * @return 0 for top, 1 for bottom, 2 for left, 3 for right, -1 if not intersecting
-     */
-    public static int getIntersectsSide(Renderable R1, Renderable R2) {
-        final int m = 1; //TODO test values
-        int res = 0;
-        //x-direction
-        if (R1.getX() + m > R2.getX() + R2.getWidth()) {
-            res |= LEFT;
-        } else if (R1.getX() + R1.getWidth() - m < R2.getX()) {
-            res |= RIGHT;
-        }
-
-        //y-direction
-        if (R1.getY() + m > R2.getY() + R2.getHeight()) {
-            res |= TOP;
-        } else if (R1.getY() + R1.getHeight() - m < R2.getY()) {
-            res |= BOTTOM;
-        }
-        return res;
-    }
-
     public static String loadResource(String fileName) {
         String result = "";
         try (InputStream in = Utils.class.getResourceAsStream(fileName);
